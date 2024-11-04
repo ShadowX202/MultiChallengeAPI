884f9184b3c5df3114451b55de1bf05c7244e544
ShadowX202 <shadow.x2029324@gmail.com>
2024-11-04 17:56:13 +0100

    Manager

--- src/main/java/de/shadowx202/multichallengeapi/ChallengeManager.java
+++ src/main/java/de/shadowx202/multichallengeapi/ChallengeManager.java
@@ -7,15 +7,13 @@ import org.bukkit.entity.Player;
 import java.util.List;
 
 public interface ChallengeManager {
-    List<Class<Challenge>> getChallenges();
+    List<Challenge> getChallenges();
     List<ChallengeData> getActiveChallenges();
 
-    boolean registerChallenge(Class<Challenge> challenge);
-
+    boolean registerChallenge(Challenge challenge);
     boolean isRegistered(Challenge challenge);
-    boolean isRegistered(Class<Challenge> challenge);
 
-    ChallengeData openChallenge(Class<Challenge> challenge, List<Player> players);
+    ChallengeData openChallenge(Challenge challenge, List<Player> players);
     ChallengeData closeChallenge(ChallengeData challengeData);
 
     List<ChallengeData> getActiveChallenges(Challenge challenge);
